df9e5ea36776728d85dbc150206d80428e64cdd1
Ginpei <ginpei@ginpei.dev>
2023-12-31 08:30:24 -0800

    first example

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+require('dotenv').config()
+
+const mysql = require('mysql2/promise')
+
+main().catch((v) => console.error(v));
+
+async function main() {
+  // Create the connection to the database
+  const connection = await mysql.createConnection(process.env.DATABASE_URL)
+  
+  // simple query
+  const [results, fields] = await connection.query('show tables');
+  console.log(results) // results contains rows returned by server
+  console.log(fields) // fields contains extra metadata about results, if available
+  
+  // Example with placeholders
+  const results2 = await connection.query('select 1 from dual where ? = ?', [1, 1]);
+  console.log(results2)
+  
+  await connection.end()
+}
